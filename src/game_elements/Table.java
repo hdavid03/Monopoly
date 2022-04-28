32fca9618ebf9c82a9cb2df8d348d9a3deb65c6b
Hajdu David <hdavid998@gmail.com>
2022-04-28 12:50:50 +0200

    branch updated

--- src/game_elements/Table.java
+++ src/game_elements/Table.java
@@ -6,10 +6,7 @@ import game_elements.Player;
 import java.util.ArrayList;
 
 public class Table extends GameElement {
-    public int miska;
     ArrayList<Player> playerArray;
 
-    public void setPlayerList(Player e) {
-       playerArray.get(3);
+
     }
-}
