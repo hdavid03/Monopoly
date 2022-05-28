ed12da4b180c881a66f3f815de1ba610945c68f7
Hajdu David <hdavid998@gmail.com>
2022-05-28 23:06:04 +0200

    .

--- src/GUI/MonopolyGUI.java
+++ src/GUI/MonopolyGUI.java
@@ -5,6 +5,7 @@ import game_elements.Player;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
